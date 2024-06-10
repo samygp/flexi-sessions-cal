c31d29c8bd1ebcfd2a09b46e8989a7b0f97dd4b3
sampai <soysamygp@gmail.com>
2024-06-10 12:08:32 -0600

    color mapping, event types and calendar context

--- src/components/CalendarioPerron/CalendarContext.ts
+++
@@ -1,8 +0,0 @@
-import { BadgeOwnProps } from '@mui/material';
-import { createContext } from 'react';
-
-export interface ICalendarContext {
-    color: BadgeOwnProps["color"],
-}
-
-export const CalendarContext = createContext({color: 'primary'});
